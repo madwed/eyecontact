65482635e16e44f5798aa1576747ae1a82a572e6
Isaac Madwed <i.madwed@gmail.com>
2015-08-07 17:12:52 -0400

    fixed typo

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ var path = require("path");
 // var logger = require("morgan");
 var ExpressPeerServer = require("peer").ExpressPeerServer;
 
-app.set("port", process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000));
+app.set("port", process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
 app.set("ip", process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 // app.use(logger("dev"));
