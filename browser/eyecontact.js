ebcd02ac4cf5a5b243c8671f48ea9b75a0dcd468
Isaac Madwed <i.madwed@gmail.com>
2015-08-07 16:45:39 -0400

    initial redshift commit

---
+++ browser/eyecontact.js
@@ -0,0 +1,212 @@
+var fps, fpsInterval, now, then, elapsed;
+fps = 30;
+
+var smoother = new Smoother([0.9999999, 0.9999999, 0.999, 0.999], [0, 0, 0, 0]),
+	localVideo = document.getElementById("localVideo"),
+	localCanvas = document.getElementById("localCanvas"),
+	remoteCanvas = document.getElementById("remoteCanvas"),
+	localCtx = localCanvas.getContext("2d"),
+	remoteCtx = remoteCanvas.getContext("2d"),
+	detector, localStream, middle;
+
+function play () {
+	compatibility.requestAnimationFrame(play);
+	//Keep on throttling
+	now = Date.now();
+    elapsed = now - then;
+    //Check if the throttle is ready and if the local video element is ready
+	if (elapsed > fpsInterval && localVideo.readyState === localVideo.HAVE_ENOUGH_DATA && localVideo.videoWidth > 0) {
+		then = now - (elapsed % fpsInterval);
+		// Prepare the detector once the localVideo dimensions are known:
+		if (!detector) {
+			var width = ~~(60 * localVideo.videoWidth / localVideo.videoHeight);
+			var height = 60;
+			detector = new objectdetect.detector(width, height, 1.1, objectdetect.frontalface);
+			// Manually set the size of the local and remote canvases
+			localCanvas.width = remoteCanvas.width = localVideo.videoWidth;
+			localCanvas.style.width = remoteCanvas.style.width = localVideo.videoWidth;
+			localCanvas.height = remoteCanvas.height = localVideo.videoHeight / 2;
+			localCanvas.style.height = remoteCanvas.style.height = localVideo.videoHeight / 2,
+			middle = localCanvas.width / 2;
+		}
+
+		// Perform the actual detection:
+		var coords = detector.detect(localVideo, 1);
+		if (coords[0]) {
+			localCtx.clearRect(0, 0, localCanvas.width, localCanvas.height);
+			var coord = coords[0];
+			coord = smoother.smooth(coord);
+
+			// Rescale coordinates from detector to localVideo coordinate space:
+			coord[0] *= localVideo.videoWidth / detector.canvas.width;
+			coord[1] *= localVideo.videoHeight / detector.canvas.height;
+			coord[2] *= localVideo.videoWidth / detector.canvas.width;
+			coord[3] *= localVideo.videoHeight / detector.canvas.height;
+
+			// Calculate eye positions from frontal face rectangle
+			var eyePosX = Math.floor(coord[0] + coord[2] / 8);
+			var eyePosY = Math.floor(coord[1] + coord[3] / 4);
+			var eyeWidth = Math.floor(coord[2] * 3 / 4);
+			var eyeHeight = Math.floor(coord[3] / 4); 
+			var middleoffset = Math.floor(middle - eyeWidth / 2);
+			//Draw the your eyes
+			localCtx.drawImage(localVideo, eyePosX, eyePosY, eyeWidth, eyeHeight, middleoffset, 0, eyeWidth, eyeHeight);
+			//If there's a connection
+			if(liveConn){
+				//Send them your eyes
+				identity.conn.send({data: localCtx.getImageData(middleoffset, 0, eyeWidth, eyeHeight).data, height: eyeHeight, width: eyeWidth});
+			}
+		}
+	}
+}
+
+function initiateEyeHole (stream) {
+	//Stream the localStream through a video element
+	//We will be applying it to the canvas later on
+	try {
+		localVideo.src = compatibility.URL.createObjectURL(stream);
+	} catch (error) {
+		localVideo.src = stream;
+	}
+	//Set up the throttle on requestAnimationFrame
+	fpsInterval = 1000 / fps;
+    then = Date.now();
+   	compatibility.requestAnimationFrame(play);
+}
+
+//Is there a connection?
+var liveConn = false;
+//Utility object to hold the client's Id, peer reference, and current connection reference
+var identity = {myId: undefined, peer: undefined, conn: undefined}
+
+//Utility method for sending http Ajax get requests
+function httpGet (url, cb) {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function () {
+		if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+			cb(JSON.parse(this.response));
+		}
+	}
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+}
+
+//When a peer DataConnection is established
+function peerDataCommunication (peerconn) {
+	identity.conn = peerconn;
+
+	//When the connection opens...
+	peerconn.on("open", function() {
+		liveConn = true;
+		//Listen for data
+		peerconn.on('data', function(data) {
+			if(data.height > 0){
+				//Clear the canvas
+				remoteCtx.clearRect(0, 0, remoteCanvas.width, remoteCanvas.height);
+				//Apply incoming ArrayBuffer to ImageData.data using a Uint8Array as the view
+				var remoteIData = remoteCtx.createImageData(data.width, data.height),
+					incomingData = new Uint8Array(data.data);
+				for(var i = 0; i < remoteIData.data.length; i++){
+					remoteIData.data[i] = incomingData[i];
+				}
+				//Figure out where to put the incoming ImageData (centered)
+				var middleoffset = Math.floor(middle - data.width / 2);
+				//Draw their eyes
+				remoteCtx.putImageData(remoteIData, middleoffset, 0);
+				remoteIData = null;
+				incomingData = null;
+			}
+		});
+	});
+
+	//If the other user closes the connection, search for another user
+	peerconn.on("close", function(){
+		console.log("closing connection");
+		peerconn.close();
+		identity.conn = undefined;
+		liveConn = false;
+		httpGet("/" + identity.myId, meetSomeone);
+	});
+
+	//If the user closes the tab, tell the other user
+	window.onbeforeunload = function () {
+		peerconn.close();
+	};
+}
+
+//Receive the response from the server, potentially with another user's peer id
+function meetSomeone (res){
+		if(res.meet === "hold"){
+			console.log("Waiting for a new friend");
+		}else{
+			console.log("Meet ", res.meet);
+			peerDataCommunication(identity.peer.connect(res.meet));
+		}
+	}
+
+//Establish peer connection with server
+function enterTheEye (env) {
+	console.log("env", env);
+	if (env === "production"){
+		identity.peer = new Peer({
+			host: "/", 
+			secure: true, 
+			port: 443, 
+			key: "peerjs", 
+			path: "/api", 
+			config: {
+				"iceServers": [{ url: "stun:stun.l.google.com:19302" }]
+			}
+		});
+	} else {
+		identity.peer = new Peer({host: "192.168.2.132", port: 3000, path: "/api", debug: 2});
+	}
+	//When the peer connection is established
+	identity.peer.on("open", function (id) {
+		console.log("my id: ", id);
+		identity.myId = id;
+		//Try to meet someone
+		httpGet("/" + id, meetSomeone);
+	});
+	//If someone calls, you answer
+	identity.peer.on("connection", function (peerconn) {
+		peerDataCommunication(peerconn);
+	})
+
+}
+		
+try {
+	//Set up the webcam
+	compatibility.getUserMedia({
+		video: {
+			mandatory: {
+				maxFrameRate: 20,
+				maxWidth: 640
+			}
+		}
+	}, function(stream) {
+		localStream = stream;
+		//When that's ready, enterTheEye and initiateEyeHole
+		httpGet("/env", enterTheEye);
+		initiateEyeHole(localStream);
+	}, function (error) {
+		console.log(error);
+		alert("WebRTC not available");
+	});
+} catch (error) {
+	alert(error);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ server/index.js
@@ -0,0 +1,63 @@
+var express = require("express");
+var app = express();
+var server = require("http").Server(app);
+var path = require("path");
+// var logger = require("morgan");
+var ExpressPeerServer = require("peer").ExpressPeerServer;
+
+app.set("port", (process.env.PORT || 3000));
+
+// app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "../browser")));
+
+app.get("/", function(req, res, next) { 
+  res.sendFile(path.join(__dirname, "./index.html")); 
+});
+
+var options = {
+    debug: true
+}
+var peerServer = ExpressPeerServer(server, options);
+
+var ids = [];
+
+peerServer.on("disconnect", function (id) {
+  var placeInLine = ids.indexOf(id);
+  if(placeInLine !== -1){
+    ids.splice(placeInLine, 1);
+  }
+});
+
+app.use("/api", peerServer);
+
+app.get("/env", function (req, res) {
+  console.log("env", process.env.NODE_ENV);
+  res.json({env: process.env.NODE_ENV});
+});
+
+app.get("/:id", function (req, res) {
+  if(ids.length > 0){
+    res.json({meet: ids.shift()});
+  }else{
+    ids.push(req.params.id);
+    res.json({meet: "hold"});
+  }
+});
+
+server.listen(app.get("port"), function () {
+  console.log("Server running at localhost: ", app.get("port"));
+})
+
+app.use(function (req, res, next) {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+app.use(function (err, req, res) {
+    res.status(err.status || 500);
+    console.log({error: err});
+    res.send(err);
+});
+
+
